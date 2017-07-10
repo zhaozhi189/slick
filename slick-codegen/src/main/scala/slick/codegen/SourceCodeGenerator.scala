1b28f89b540a1f1870c359e113d32cb3557593e0
zzho <zhi.zhao@eoitek.com>
2017-07-10 16:36:08 +0800

    denerator my code

--- slick-codegen/src/main/scala/slick/codegen/SourceCodeGenerator.scala
+++ slick-codegen/src/main/scala/slick/codegen/SourceCodeGenerator.scala
@@ -1,60 +1,71 @@
 package slick.codegen
 
-import java.net.URI
-
-import scala.concurrent.{Await, ExecutionContext}
-import scala.concurrent.duration.Duration
-import slick.basic.DatabaseConfig
+import slick.driver.JdbcProfile
 import slick.{model => m}
-import slick.jdbc.JdbcProfile
-import slick.model.Model
-import slick.util.ConfigExtensionMethods.configExtensionMethods
+
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, ExecutionContext}
 
 /**
- * A customizable code generator for working with Slick.
- *
- * For usage information please see the corresponding part of the Slick documentation.
- *
- * The implementation is structured into a small hierarchy of sub-generators responsible
- * for different fragments of the complete output. The implementation of each
- * sub-generator can be swapped out for a customized one by overriding the corresponding
- * factory method. SourceCodeGenerator contains a factory method Table, which it uses to
- * generate a sub-generator for each table. The sub-generator Table in turn contains
- * sub-generators for Table classes, entity case classes, columns, key, indices, etc.
- * Custom sub-generators can easily be added as well.
- *
- * Within the sub-generators the relevant part of the Slick data `model` can
- * be accessed to drive the code generation.
- *
- * Of coures it makes sense to integrate this into your build process.
- * @param model Slick data model for which code should be generated.
- */
+  * A customizable code generator for working with Slick.
+  *
+  * For usage information please see the corresponding part of the Slick documentation.
+  *
+  * The implementation is structured into a small hierarchy of sub-generators responsible
+  * for different fragments of the complete output. The implementation of each
+  * sub-generator can be swapped out for a customized one by overriding the corresponding
+  * factory method. SourceCodeGenerator contains a factory method Table, which it uses to
+  * generate a sub-generator for each table. The sub-generator Table in turn contains
+  * sub-generators for Table classes, entity case classes, columns, key, indices, etc.
+  * Custom sub-generators can easily be added as well.
+  *
+  * Within the sub-generators the relevant part of the Slick data `model` can
+  * be accessed to drive the code generation.
+  *
+  * Of coures it makes sense to integrate this into your build process.
+  *
+  * @param model Slick data model for which code should be generated.
+  */
 class SourceCodeGenerator(model: m.Model)
-                   extends AbstractSourceCodeGenerator(model) with OutputHelpers{
+  extends AbstractSourceCodeGenerator(model) with OutputHelpers {
   // "Tying the knot": making virtual classes concrete
   type Table = TableDef
   def Table = new TableDef(_)
-  class TableDef(model: m.Table) extends super.TableDef(model){
-    // Using defs instead of (caching) lazy vals here to provide consitent interface to the user.
-    // Performance should really not be critical in the code generator. Models shouldn't be huge.
-    // Also lazy vals don't inherit docs from defs
-    type EntityType     =     EntityTypeDef
-    def  EntityType     = new EntityType{}
-    type PlainSqlMapper =     PlainSqlMapperDef
-    def  PlainSqlMapper = new PlainSqlMapper{}
-    type TableClass     =     TableClassDef
-    def  TableClass     = new TableClass{}
-    type TableValue     =     TableValueDef
-    def  TableValue     = new TableValue{}
-    type Column         =     ColumnDef
-    def  Column         = new Column(_)
-    type PrimaryKey     =     PrimaryKeyDef
-    def  PrimaryKey     = new PrimaryKey(_)
-    type ForeignKey     =     ForeignKeyDef  
-    def  ForeignKey     = new ForeignKey(_)
-    type Index          =     IndexDef  
-    def  Index          = new Index(_)
+
+  class TableDef(model: m.Table) extends super.TableDef(model) {
+    type EntityType = EntityTypeDef
+
+    def EntityType = new EntityType {}
+
+    type PlainSqlMapper = PlainSqlMapperDef
+
+    def PlainSqlMapper = new PlainSqlMapper {}
+
+    type TableClass = TableClassDef
+
+    def TableClass = new TableClass {}
+
+    type TableValue = TableValueDef
+
+    def TableValue = new TableValue {}
+
+    type Column = ColumnDef
+
+    def Column = new Column(_)
+
+    type PrimaryKey = PrimaryKeyDef
+
+    def PrimaryKey = new PrimaryKey(_)
+
+    type ForeignKey = ForeignKeyDef
+
+    def ForeignKey = new ForeignKey(_)
+
+    type Index = IndexDef
+
+    def Index = new Index(_)
   }
+
 }
 
 /** A runnable class to execute the code generator without further setup */
